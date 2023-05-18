f196699fd0076ce92fb4bf1b9297346cf1f7ad69
rahul2004a <kumar.rahul.2017354@gmail.com>
2023-05-18 21:53:35 +0530

    First Commit

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+export {default as Navbar} from './Navbar';
+export {default as SearchFeed} from './SearchFeed';
+export {default as VideoDetails} from './VideoDetails';
+export {default as Feed} from './Feed';
+export {default as ChannelDetails} from './ChannelDetails';
+export {default as SearchBar} from './SearchBar';
+export {default as Sidebar} from './Sidebar';
+
+
