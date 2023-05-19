e73072b680b3d473e5b5a2e28968bab950f402e0
rahul2004a <kumar.rahul.2017354@gmail.com>
2023-05-19 21:31:59 +0530

    UPdate in index

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,6 @@ export {default as Feed} from './Feed';
 export {default as ChannelDetails} from './ChannelDetails';
 export {default as SearchBar} from './SearchBar';
 export {default as Sidebar} from './Sidebar';
+export {default as Sidebar} from './Sidebar';
 
 
