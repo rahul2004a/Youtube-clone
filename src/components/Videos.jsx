0596a4a3de3179e92f1f8b44cd226c593429d172
rahul2004a <kumar.rahul.2017354@gmail.com>
2023-05-22 10:14:09 +0530

    Make Search Feed

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Stack,Box } from '@mui/material';
 import {VideoCard,ChannelCard} from './';
-const Videos = ({videos}) => {
+const Videos = ({videos,direction}) => {
   // console.log(videos);
+  if(!videos?.length)
+  return 'Loading...';
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack direction={direction||"row"} flexWrap="wrap" justifyContent="start" gap={2}>
       {videos && videos.map((item,idx)=>(
+        console.log(item.id.channelId),
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item}/>}
-          {/* {item.id.channelId && <ChannelCard channelDetail={item}/>} */}
+          {item.id.channelId && <ChannelCard channelDetail={item}/>}
 
         </Box>
       ))}
